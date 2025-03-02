88915f7677b08f26023fa44df4177850413ba03c
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-03-03 05:02:39 +0530

    Delete example_llm directory

--- example_llm/chatbot-app/src/components/ChatHeader.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ChatHeader = ({ title }) => (
-  <div className="bg-purple-700 p-4 text-center text-white font-bold text-lg">
-    {title}
-  </div>
-);
-
-export default ChatHeader;
