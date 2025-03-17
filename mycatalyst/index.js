93c0580bdb22b2a69b6f18ea783bf7abd96708f1
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-03-17 19:45:04 +0530

    Delete mycatalyst directory

--- mycatalyst/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.redirect("/server"); // Redirect to FastAPI function
-});
-
-module.exports = app;
